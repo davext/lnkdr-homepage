02db88312f10b97eb3dedbca0beb99330efc66ba
David Harvey <18369214+davext@users.noreply.github.com>
2023-11-17 12:16:56 -0800

    feature: added reset button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Spline from '@splinetool/react-spline';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 
 function App() {
 
   const [loading, setLoading] = useState(true)
+  const [showReset, setShowReset] = useState(false)
+
+  const spline = useRef();
+
+  function onLoad(splineApp) {
+    spline.current = splineApp;
+  }
 
   useEffect(() => {
     //load for 1 second
@@ -13,17 +20,37 @@ function App() {
     }, 800)
   }, [])
 
+  useEffect(() => {
+    //on mouse up show reset button
+    window.addEventListener('mouseup', () => {
+      setShowReset(true)
+    })
+  }, [])
+
   return (
     <>
       {loading && <div style={{ width: '100vw', height: '100vh', backgroundColor: 'black', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <div className="loading-skeleton"></div>
       </div>}
       <div>
+        <button type="button"
+          style={{
+            position: 'fixed',
+            top: '10px',
+            left: '10px',
+            zIndex: 2,
+            opacity: showReset ? 1 : 0,
+          }}
+          onClick={() => {
+            window.location.reload()
+          }}>
+          Reset
+        </button>
         <Spline
+          onLoad={onLoad}
           style={{ width: '100vw', height: '100vh' }}
           scene="https://prod.spline.design/3k-MHhb3DXVBm22Z/scene.splinecode"
         />
-
       </div>
     </>
   )
